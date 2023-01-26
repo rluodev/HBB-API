14a25725967cddfda12ec53bf98ec5ab216fca12
Soar1826 <34289582+Soar1826@users.noreply.github.com>
2023-01-26 12:24:13 -0800

    Create generateKeyPair.cjs

---
+++ tools/generateKeyPair.cjs
@@ -0,0 +1,7 @@
+//import crypto from 'crypto';
+const crypto = require('crypto');
+
+const randomBytes = crypto.randomBytes(64).toString('hex');
+const hash = crypto.createHash('sha512').update(randomBytes).digest('hex');
+
+console.log(`API key:           hoc-m-${randomBytes}\nServer-side hash:  HOC-M-${hash.toUpperCase()}`);
